9993d0d9991c034f3c4450e6e91dfbf3dc623b46
omar hassan <omarhassan00123@gmail.com>
2023-08-25 00:49:39 +0000

    a

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/*
+*list_len : mark the length of linked list
+*@c: ptr to first node
+*
+* return: size of list
+*/
+
+size_t list_len(const list_t *c)
+{
+	size_t r = 0;
+
+	while (c)
+	{
+		c = c->next;
+		r++;
+	}
+	return (r);
+}
