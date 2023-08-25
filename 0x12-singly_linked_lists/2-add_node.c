6b29e267ed044fa25fbcbf4cf9183d470f885322
omar hassan <omarhassan00123@gmail.com>
2023-08-25 01:09:03 +0000

    a

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -13,14 +13,14 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new_head = malloc(sizeof(list-t));
 
 	if (!head || !new_head)
-		return (0);
+		return (null);
 	if (str)
 	{
 		new_head->str = strdup(str);
 		if (!new_head->str)
 		{
 			free(new_head);
-			return (0);
+			return (null);
 		}
 		new_head->len = _strlen(new_head->str);
 	}
