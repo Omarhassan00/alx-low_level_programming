5da2c2f1a265562ccbfcfb0b4291f4e59b9a1fc9
omar hassan <omarhassan00123@gmail.com>
2023-08-25 00:07:02 +0000

    a

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -24,7 +24,7 @@ typedef struct list_s
  size_t print_list(const list_t *c);
 size_t list_len (const list_t *c);
 list_t *add_node(list_t **head, const char *ptr);
-list_t *add_node_end(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *ptr);
 void free_list(list_t *head);
 void _constructor(void) _attribute_ ((constructor));
 #endif
