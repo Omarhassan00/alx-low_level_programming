dcdbb73f327a62f6567044563b805d3fa0d497c0
omar hassan <omarhassan00123@gmail.com>
2023-08-25 00:32:52 +0000

    a

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,35 +1,40 @@
 #include "lists.h"
+
 /*
- * strlen - returns length of string
- * @v: string who length to check
- *
- * return: int length of string
- */
+* _strlen: - returns length of string
+* @v: string who length to check
+*
+* return: int length of string
+*/
 
 int _strlen(char *v)
+
 {
-	int r = 0 ;
-	if(!v)
-		return 0;
+	int r = 0;
+
+	if (!v)
+		return (0);
 	while (*v++)
 		r++;
-	return r;
+	return (r);
 }
+
 /*
- * print_list - linked list
- * @c: pointer to first node
- *
- * return: size of list
- */
+* print_list: - linked list
+* @c: pointer to first node
+*
+* return: size of list
+*/
 
 size_t print_list(const list_t *c)
 {
-	size_t r = 0 ;
-	while(c)
+	size_t r = 0;
+
+	while (c)
 	{
-		printf("[%d] %s\n",_strlen(c->str),c->str ? c->str : "(nil)");
+		printf("[%d] %s\n", _strlen(c->str), c->str ? c->str : "(nil)");
 		c = c->next;
 		r++;
 	}
-	return r;
+	return (r);
 }
